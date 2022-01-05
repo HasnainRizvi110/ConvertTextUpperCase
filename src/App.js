3b8c86495b141f3d9ecec8b67f1a917ca7cc473c
HasnainRizvi <HasnainRizvi.Git@gmail.com>
2022-01-06 00:39:14 +0500

    ClearText-CopyText-RemoveExtraSpaces-Course-11-also-Convert-Lower-Upper-Case-Course11

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 
 
 
-<About/>
+<About/> 
 </div>
 
 
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,10 +20,28 @@ export default function TextForm(props) {
         // let newText = text.toLowerCase();
         setText(newText)
     }
+    
+
+    const handleClearClick = ()=>{
+        let newText = '';
+        setText(newText)
+    }
 
     const handleOnChange = (event)=>{
         // console.log("OnChange");
         setText(event.target.value);
+    }
+
+    const handleCopy = () =>{
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[  ]+/);
+        setText(newText.join(" "))
     }
 
     const [text, setText] = useState('Enter Text Here');
@@ -37,6 +55,9 @@ export default function TextForm(props) {
 </div>
 <button className="btn btn-primary mx-1" onClick={handleUpClick} >Convert To UpperCase</button>
 <button className="btn btn-danger mx-1" onClick={handleLoClick} >Convert To LowerCase</button>
+<button className="btn btn-danger mx-1" onClick={handleClearClick}>Clear Text</button>
+<button className="btn btn-danger mx-1" onClick={handleCopy} >Copy Text</button>
+<button className="btn btn-danger mx-1" onClick={handleExtraSpaces} >Remove Extra Spaces</button>
         </div>
 
 
